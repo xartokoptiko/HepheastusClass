5461ede1ce2da63418c3fd31f87daeafcb4caee3
lampros <lamproskarachristos@yahoo.com>
2024-05-30 19:31:01 +0300

    Added home screen

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -11,9 +11,12 @@ module.exports = {
                 "red" : "#ff0e36",
                 "customPurple": '#9200AA',
                 "customGray": '#2D2D2D',
+                "lightPurple": "#B155CC"
               },
               backgroundImage: {
                       'custom-gradient': 'linear-gradient(135deg, #9200AA 0%, #2D2D2D 70%)',
+                      'hover-gradient': 'linear-gradient(135deg, #9200AA 0%, #B155CC 70%)',
+                      'hover-gradient-reverse': 'linear-gradient(135deg, #B155CC 0%, #9200AA 70%)',
                     },
     },
   },
